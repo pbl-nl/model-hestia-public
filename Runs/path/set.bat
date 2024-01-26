b5f411274bd9d71133c652b0fe288d77fd442b37
MolenvdF <folckert.vandermolen@pbl.nl>
2024-01-26 13:45:52 +0100

    Overzetten Hestia code van SAWEC-WP5-v14.0.0

---
+++ Runs/path/set.bat
@@ -0,0 +1,11 @@
+REM aanpassingen voor machine specifieke paden
+
+set prj_dir=C:\prj\pbl\SawecPerfTest
+set log_dir=%prj_dir%\log
+set exe_dir=C:\Program Files\ObjectVision\GeoDms8032
+
+REM einde aanpassingen voor machine specifieke paden
+
+set exe_path=%exe_dir%\GeoDMSRun.exe
+
+
